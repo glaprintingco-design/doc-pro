40941b61841435e16ae1f76de823d6327d64efff
glaprintingco-design <glaprinting.co@gmail.com>
2026-03-07 09:18:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ with tabs[0]:
 
         st.divider()
 
-        st.markdown("### 2️⃣ Add Devices <span style='color:gray; font-size:14px;'>(Optional)</span>", unsafe_allow_html=True)
+        st.markdown("### 2️⃣ Add Devices <span style='color:gray; font-size:14px;'>(A-433 Optional)</span>", unsafe_allow_html=True)
 
         floor    = st.selectbox("Floor Location", main.FULL_FLOOR_LIST)
         category = st.selectbox("Category", list(main.MASTER_DEVICE_LIST.keys()))
