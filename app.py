30f35cd387e727d760c5d48c29ed39548bed6d92
glaprintingco-design <glaprinting.co@gmail.com>
2026-03-09 13:34:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -268,12 +268,11 @@ def login_ui_centered():
     with col2:
         # Tarjeta blanca central
         st.markdown("""
-        <div style="background-color: white; padding: 3rem; border-radius: 16px; box-shadow: 0 10px 25px rgba(0,0,0,0.05); border-top: 5px solid #FF6B00; margin-top: 5vh; text-align: center;">
         """, unsafe_allow_html=True)
         
         if os.path.exists("logo.png"):
             st.image("logo.png", width=250)
-            st.markdown("<p style='color: #718096; font-size: 15px; margin-top: -75px; margin-bottom: 2rem;'>Automated form generation for the NYC Fire Alarm Industry</p>", unsafe_allow_html=True)
+            st.markdown("<p style='color: #718096; font-size: 15px; margin-top: -90px; margin-bottom: 2rem;'>Automated form generation for the NYC Fire Alarm Industry</p>", unsafe_allow_html=True)
         else:
             st.markdown("<h1 style='color: #FF6B00; margin-bottom: 0;'>🔥 Fire Form Pro</h1>", unsafe_allow_html=True)
             st.markdown("<p style='color: #718096; font-size: 15px; margin-bottom: 2rem;'>Automated form generation for the NYC Fire Alarm Industry</p>", unsafe_allow_html=True)
